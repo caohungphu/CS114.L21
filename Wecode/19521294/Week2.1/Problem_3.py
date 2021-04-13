692c63cfbce9aa4147c1d979565d3df8a2e9d6b8
Nguyễn Thành Công <58556683+Tfushimura@users.noreply.github.com>
2021-04-13 15:10:42 +0700

    Delete Wecode/19521294/Week2.1 directory

--- Wecode/19521294/Week2.1/Problem_3.py
+++
@@ -1,8 +0,0 @@
-import sys
-input = sys.stdin.readline
-n = input()
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-a += b
-a.sort()
-sys.stdout.write(' '.join(map(str, a))+"\n")
