feb02624f62664178509bdff7785ff320cb2a1f3
Hung Phu Cao <caohungphuvn@gmail.com>
2021-06-14 08:01:37 +0700

    Upload new Json

---
+++ Colab/CSV/Sarcasm_Detection/CSVToJson.py
@@ -0,0 +1,13 @@
+# Source: https://stackoverflow.com/questions/19697846/how-to-convert-csv-file-to-multiline-json
+
+import csv
+import json
+
+csvfile = open('Sarcasm_Detection_Full.csv', 'r', encoding="UTF-8")
+jsonfile = open('Sarcasm_Detection_Full.json', 'w', encoding="UTF-8")
+
+
+reader = csv.DictReader(csvfile)
+for row in reader:
+    json.dump(row, jsonfile)
+    jsonfile.write('\n')
